f1c432ad3e4523a1b866ed4c0f493c8c09fc6622
sophiee <105938870+sophiell27@users.noreply.github.com>
2023-11-19 00:55:15 +0800

    remove: remove unuse code

--- src/pages/HomePage/hooks/useModal.tsx
+++ src/pages/HomePage/hooks/useModal.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const useModal = () => {
-    const [isOpenModal, setIsopenModal] = useState(true);
-    const toggleModal = () => {
-       return setIsopenModal(!isOpenModal);
-    };
-    return {
-        isOpenModal,
-        toggleModal
-    };
+  const [isOpenModal, setIsopenModal] = useState(true);
+  const toggleModal = () => {
+    return setIsopenModal(!isOpenModal);
+  };
+  return {
+    isOpenModal,
+    toggleModal,
+  };
 };
 
 export default useModal;
